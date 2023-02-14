6047baa71b0593376d2c93845efa852659631766
CofAndrea <andreacoff@hotmail.it>
2023-02-14 16:47:17 +0100

    first_upload
    first publication of this repo

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# js_project
- A simple js counter
+CountMe
+A simple java application that allows the user to increase and decrease the value of the counter by one or by ten and give the option to reset value to 0.
+
+Description
+The user displays 0 as the value of the counter and has five buttons +/- +10/-10 and "reset" to change the value of the counter.
+
+The counter layout is minimalistic and clean.
+
+Link
+
+
+Author
+Andrea Coffett
+(https://twitter.com/andrea_coffetti) (https://github.com/CofAndrea)
+
+Version History
+0.1
+Initial Release
---
+++ counter_project/assets/js/script.js
@@ -0,0 +1,26 @@
+const counter = document.querySelector("#counter")
+const btns = document.querySelectorAll(".btn")
+
+let count = 0
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) =>{
+        const styles = e.currentTarget.classList
+        if(styles.contains("increase")){
+            count++
+        } else  if(styles.contains("decrease")){
+            count--
+            
+        } else  if(styles.contains("decrease_ten")){
+            count -=10
+            
+        } else  if(styles.contains("increase_ten")){
+            count +=10
+            
+        } 
+        else {
+            count = 0
+        }
+        counter.textContent = count;
+
+    })
+});
